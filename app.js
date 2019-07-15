dbed7889d73cbcfb9c36ad924cd8c01ffa9badec
cristiansilva143 <cristiansilva143@gmail.com>
2019-07-15 20:20:11 -0300

    ultimo commit

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use('/', index);
 
+app.use(function(err, req, res, next) {
+    console.error(err.message); 
+    if (!err.statusCode) err.statusCode = 400;
+
+    res.status(err.statusCode).send(err.message); 
+  });
+
+
+
+
 app.listen(3000, function(){
     console.log('Aplicacion corriendo en el port 3000!!')
 }) ;    
